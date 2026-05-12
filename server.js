bcea6f2ffe8af6d692943c47c393c0f7812cfb95
giftsvillageqatif <giftsvillageqatif@gmail.com>
2026-05-12 23:38:19 +0300

    Update server.js

--- server.js
+++ server.js
@@ -14,6 +14,10 @@ const bot = new TelegramBot(process.env.TELEGRAM_TOKEN, {
   polling: true
 });
 
+bot.on("polling_error", (error) => {
+  console.log("❌ POLLING ERROR:", error);
+});
+
 
 const AUTH_PASSWORD = process.env.BOT_PASSWORD;
 
